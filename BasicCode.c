c6adaf887b728050a9aae2f878bc53c9780673d2
Arash-BozorgChenani <145478046+Arash-BozorgChenani@users.noreply.github.com>
2023-10-07 18:35:09 +0000

    updated the code

--- BasicCode.c
+++ BasicCode.c
@@ -2,12 +2,37 @@
 #include <math.h> // to use the math library you need to link the compiler with mth library using gcc Code.c -lm
 
 int main(){
-float radius, Area;
-const float Pi=3.14;
-printf("Please Enter the Radius...\n");
-scanf("%f", &radius);
-Area=Pi*pow(radius,2);
-printf("The area of the circle is: %.2f\n", Area);
+int i, n, factoriel=1, k=0;
+scanf("%d", &n);
+for (i=1;i<=n;i++){
+factoriel*=i;
+K=factoriel*2;
+}
+printf("Factoriel is %d \n", factoriel);
+
+
+// Ctrl and K+C to comment out, Ctrl and K+U to uncomment
+// float Fahrenheit, Celcius;
+// scanf("%f", &Fahrenheit);
+// Celcius=(Fahrenheit-32)*5/9;
+// printf("It is %.2f Celcius\n", Celcius);
+
+//int n;
+//scanf("%d", &n);
+//if (n%2==0){
+//printf("%d is even\n", n);
+//}else
+//{
+//printf("%d is odd\n", n);
+//}
+
+
+//float radius, Area;
+//const float Pi=3.14;
+//printf("Please Enter the Radius...\n");
+//scanf("%f", &radius);
+//Area=Pi*pow(radius,2);
+//printf("The area of the circle is: %.2f\n", Area);
 
 //printf("Size of int: %zu bytes\n", sizeof(int) );
 //printf("Size of float: %zu bytes\n", sizeof(float) );
